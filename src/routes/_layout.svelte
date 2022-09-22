2818c3e6c3ee21ce3a6b947aa653ed019c945f92
firebat75 <jacklowry1337@gmail.com>
2022-09-22 09:01:57 -0400

    added footer and dynamic styling per segment in Nav

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,22 +1,24 @@
 <script>
-	import Nav from '../components/Nav.svelte';
+	import Nav from "../components/Nav.svelte";
+	import Footer from "../components/Footer.svelte";
 
 	export let segment;
 </script>
 
+<Nav {segment} />
+
+<main>
+	<slot />
+</main>
+
+<Footer />
+
 <style>
 	main {
 		position: relative;
-		max-width: 56em;
+		max-width: 960px;
 		background-color: white;
-		padding: 2em;
+		padding: 6px 10px;
 		margin: 0 auto;
-		box-sizing: border-box;
 	}
 </style>
-
-<Nav {segment}/>
-
-<main>
-	<slot></slot>
-</main>
